8c7231075345365064c3f885890c4243519641b8
Michal Policht <michpolicht@gmail.com>
2018-02-02 02:08:35 +0100

    Add run source code test.

--- server/handlers_test.go
+++ server/handlers_test.go
@@ -2,6 +2,7 @@ package main
 
 import (
 	"bytes"
+	"encoding/binary"
 	"net/http"
 	"net/http/httptest"
 	"testing"
@@ -26,4 +27,39 @@ func TestAddSourceCodeHandler(t *testing.T) {
 		t.Errorf("Handler AddSourceCode returned wrong status code: got %v want %v", status, http.StatusOK)
 	}
 
+}
+
+func TestRunSourceCodeHandler(t *testing.T) {
+    const VALID_CODE_ID = 0
+    const NONEXISTING_CODE_ID = 1
+
+    sourceCodesMap[VALID_CODE_ID] = "tests/validCode.go"
+    sourceCodesMap[NONEXISTING_CODE_ID] = "tests"
+
+
+    rr := runSourceCodeRequest(t, NONEXISTING_CODE_ID)
+	if status := rr.Code; status != http.StatusBadRequest {
+		t.Errorf("Handler RunSourceCode returned wrong status code: got %v want %v", status, http.StatusBadRequest)
+	}
+
+    rr = runSourceCodeRequest(t, VALID_CODE_ID)
+	if status := rr.Code; status != http.StatusOK {
+		t.Errorf("Handler RunSourceCode returned wrong status code: got %v want %v", status, http.StatusOK)
+	}
+}
+
+func runSourceCodeRequest(t *testing.T, sourceCodeId uint64) *httptest.ResponseRecorder {
+    idBuf := make([]byte, binary.MaxVarintLen64)
+    binary.PutUvarint(idBuf, sourceCodeId)
+
+	req, err := http.NewRequest("POST", "/run_source_code", bytes.NewBuffer(idBuf))
+	if err != nil {
+		t.Fatal(err)
+	}
+
+	rr := httptest.NewRecorder()
+	handler := http.HandlerFunc(RunSourceCode)
+
+	handler.ServeHTTP(rr, req)
+	return rr
 }
